2d4772655b0fae53a770d7a291251e7f27507cfd
Keegan Christie <kchristi@mail.walshcollege.edu>
2018-10-28 10:53:17 -0400

    First four requirements

---
+++ Variables_Assignment.py
@@ -0,0 +1,13 @@
+# First requirement storing first name in all lowercase
+string_first = 'keegan'
+
+#Second and Fourth requirement 2 new lines and last name in all uppercase
+string_lastname = "CHRISTIE\n\n"
+
+string_quote = "\"Start by doing what's necessary; then do what's possible; and suddenly you are doing the impossible - Francis of Assisi\""
+
+#Third requirement 
+print("Hello, " + string_first.upper() + " " + string_lastname.lower())
+
+print(string_quote)
+
---
+++ stringtest.py
@@ -0,0 +1,6 @@
+string_var = 'hello\n\t'
+
+string_name = "Keegan Christie"
+
+print(string_var  + string_name)
+
