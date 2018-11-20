0a4e607f324db9bdbb8f0818b5fdd6b2e96217f8
Keegan Christie <kchristi@mail.walshcollege.edu>
2018-11-20 18:06:33 -0500

    To try more things

--- Week 8/functions assignment.py
+++ Week 8/functions assignment.py
@@ -3,7 +3,7 @@ employee_full = []
 flag = False
 def new_employee(employee_ID, employee_Name, employee_address, employee_email):
     employee_list.append({'Employee ID': employee_ID, 'Employee Name': employee_Name, 'Employee Email': employee_email, 'Employee Address' : employee_address})
-
+    return employee_list
 
 while not flag:
     employee_ID = input("Please enter the employee's ID ")
@@ -11,11 +11,13 @@ while not flag:
     employee_address = input("Please enter the employees address ")
     employee_email = input("Please enter the employees email ")
 
-    employee_full = new_employee(employee_ID, employee_Name, employee_address, employee_email)
-    
+
     escape = input("Would you like to enter another employee?(Y/N) ")
 
     if escape == "Y":
         continue
     else:
         break
+    employee_list = new_employee(employee_ID, employee_Name, employee_address, employee_email)
+
+print(employee_list)
